8e6c18ccc7311bfa4bee1b824a6b3cbcd1065f0f
krunal-mojidra-07 <mojidrakrunal8467@gmail.com>
2026-05-22 04:49:43 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use Strict"; // treat all js code as a newer version
+
+console.log(3 + 3);
+
+//code readaibility should be high.
+
+console.log("Krunal");
+
+
+let name = "Krunal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// datatype
+
+//number => 2 to power 53
+//bigint for  large number
+//string => ""
+//boolean => true/false
+//unddefined => not defined anything
+//null => standalone value
+//Symbol => unique 
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
