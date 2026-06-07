6bfd633ac469f916675266f9cd4c7fab7818f110
krunal-mojidra-07 <mojidrakrunal8467@gmail.com>
2026-06-07 07:02:12 +0000

    value comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+// avoid upper two null & undefined for code consistansy
+
+// === for strickly check & check data type too.
+
+console.log("2" === 2);
+console.log(2 === 2);
