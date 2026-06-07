a3a0a71eacf2c2aa84444b8893749957b6c5a0e6
krunal-mojidra-07 <mojidrakrunal8467@gmail.com>
2026-06-07 06:27:37 +0000

    operation convertion

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,69 @@
+/*let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+// "33" => 33
+// true => 1; false => 0
+//"33abc" => NaN
+
+/*let isLoggedIn = "Krunal"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "Krunal" => false
+*/
+
+/*
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+*/
+
+// ***************** Operation *****************
+
+/*
+let str1 = "Hello "
+let str2 = " Krunal "
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 2);
+console.log(2 % 2);
+*/
+
+//console.log(+true);
+//console.log(+"");
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2  // not prefer for code readaibility
+
+let gameCounter = 100
+gameCounter++;
+// ++ gameCounter  example of prefix and postfix
+console.log(gameCounter);
+
+// for learn prefix and postfix and practice it go to website MDN.
