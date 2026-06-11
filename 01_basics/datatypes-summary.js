2e06753da54d1c5176c8d57a738234f50f7de680
krunal-mojidra-07 <mojidrakrunal8467@gmail.com>
2026-06-11 04:27:32 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,55 @@
+// For Interview And Projects Creations.
+// There are two type of data types :1.Primitive and 2.Non primitive(refference datatype)
+
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const otherId = Symbol('123')
+
+console.log(id === otherId);
+
+const bigNumber = 3215968551354654n
+
+
+
+// refference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj", "Doga"];
+let myObj = {
+    name: "Krunal",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World...");
+    
+}
+
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof id);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myFunction);
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive), Heap (Non Primitive)
